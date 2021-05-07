fe774632fc25c673f05fdf7c88e4582ef8a4c5cb
QUDUSKUNLE <quduskunle@gmail.com>
2021-05-07 09:41:41 +0100

    Link entities to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
+import { Company } from './companies/entities/company.entity';
 import { CompaniesModule } from './companies/companies.module';
 import { CompaniesResolver } from './companies/companies.resolver';
 import { CompaniesService } from './companies/companies.service';
+import { Employee } from './employees/entities/employee.entity';
 import { EmployeesModule } from './employees/employees.module';
 import { EmployeesResolver } from './employees/employees.resolver';
 import { EmployeesService } from './employees/employees.service';
@@ -22,7 +24,8 @@ import { EmployeesService } from './employees/employees.service';
       username: 'postgres',
       password: 'Boluwatife08971',
       database: 'heyday',
-      synchronize: false,
+      entities: [Company, Employee],
+      synchronize: true,
     }),
     CompaniesModule,
     EmployeesModule,
